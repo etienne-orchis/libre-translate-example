6b9897be0ec6755e73af24ad892c6e946474b4fb
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-03-20 11:53:22 +0300

    using the translation service in the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TranslationService } from './services/translation.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [FormsModule],
+  template: `
+    <input [(ngModel)]="text" placeholder="Entrez du texte" />
+    <button (click)="translate()">Traduire</button>
+    <p>Traduction en anglais: {{ translatedText }}</p>
+  `,
 })
 export class AppComponent {
-  title = 'libre-translate-example';
+  text = '';
+  translatedText = '';
+
+  constructor(private readonly translationService: TranslationService) {}
+
+  translate() {
+    this.translationService
+      .translateText(this.text, 'en')
+      .subscribe((response) => {
+        this.translatedText = response.translatedText;
+      });
+  }
 }
