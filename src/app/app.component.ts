554100bc34d073b892f8298ab739bc4baa8daf9e
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-03-21 08:27:16 +0300

    adding a loading state for a better UX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,32 @@ import { FormsModule } from '@angular/forms';
   template: `
     <input [(ngModel)]="text" placeholder="Entrez du texte" />
     <button (click)="translate()">Traduire</button>
+    @if(loading){
+    <p>Traduction en cours ...</p>
+    }@else {
     <p>Traduction en anglais: {{ translatedText }}</p>
+    }
   `,
 })
 export class AppComponent {
   text = '';
   translatedText = '';
+  loading: boolean | undefined = undefined;
 
   constructor(private readonly translationService: TranslationService) {}
 
   translate() {
-    this.translationService
-      .translateText(this.text, 'en')
-      .subscribe((response) => {
+    this.loading = true;
+    this.translatedText = '';
+    this.translationService.translateText(this.text, 'en').subscribe({
+      next: (response) => {
         this.translatedText = response.translatedText;
-      });
+        this.loading = false;
+      },
+      error: (e) => {
+        this.loading = false;
+        console.error(e);
+      },
+    });
   }
 }
